75858e71169204d2bcd899d25b84f54fa32bede0
livakivska <livakivska@gmail.com>
2021-12-12 23:23:22 +0100

    added redirect to router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,17 @@
-export const routes = [
-    {
-      path: "",
-      name: "search-movies",
-      component: () => import("@/views/SearchMovies"),
+export const routes = [{
+        path: "",
+        name: "search-movies",
+        component: () => import("@/views/SearchMovies"),
     },
     {
-      path: '/watched-movies',
-      name: 'watched-movies',
-      component: () => import("@/views/WatchedMovies.vue")
+        path: '/watched-movies',
+        name: 'watched-movies',
+        component: () => import("@/views/WatchedMovies.vue")
+    },
+    {
+        path: "*",
+        redirect: {
+            name: "search-movies"
+        },
     }
-]; 
+];
